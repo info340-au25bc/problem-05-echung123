ba4929f3a58aad9ef892f359d43cce39cae93589
echung123 <echung23@uw.edu>
2025-10-23 15:58:53 -0700

    problem a finished

--- problem-a/js/index.js
+++ problem-a/js/index.js
@@ -11,7 +11,7 @@ console.log(motto);
 //Define a variable `mottoLength` that stores the length of the motto.
 //Use the `.length` property
 //Log out the length.
-let mottoLength = motto.length;
+const mottoLength = motto.length;
 console.log(mottoLength);
 
 //Use the `indexOf()` String method to see if the word "cool" is in the string.
@@ -30,8 +30,8 @@ console.log(motto);
 //the old (which you had saved in a variable!). Log out this ratio as a 
 //percentage with two decimal places of precision (e.g., `"123.45%"`).
 //You can use the `.toFixed()` Number method to specify the precision. 
-let lengthRatio = (motto.length/mottoLength) *100;
-console.log(lengthRatio.toFixed(2) +"%");
+const ratio = (motto.length/mottoLength) *100;
+console.log(ratio.toFixed(2) +"%");
 
 
 /** Arrays **/
@@ -39,7 +39,7 @@ console.log(lengthRatio.toFixed(2) +"%");
 //Create an array `numbers` that contains these 10 numbers:
 //    1 4 1 5 9 2 6 5 3 5
 //Log out the array.
-let numbers = [ 1,4,1,5,9,2,6,5,3,5];
+const numbers = [ 1,4,1,5,9,2,6,5,3,5];
 console.log(numbers);
 
 
@@ -58,8 +58,8 @@ console.log(numbers);
 //You can use the `Math.floor()` function to round to a whole number.
 //Log out the median value.
 numbers.sort();
-let middleIndex = Math.floor(numbers.length/2);
-let median= numbers[middleIndex];
+const middleIndex = Math.floor(numbers.length/2);
+const median= numbers[middleIndex];
 console.log(median);
 
 
@@ -69,18 +69,18 @@ console.log(median);
 //with properties:
 //  `x` (coordinate) of 30, `y` of 50, `width` of 100, `height` of 50
 //Log out the rectangle object
-let rect = {x:30,y:50,width:100,height:50};
+const rect = {x:30, y:50, width:100, height:50};
 console.log(rect);
 
 
 //Log out the x- and y- coordinates of the rectangle (its location). Your output 
 //should have the format `"X, Y"`.
-console.log(rect['x']+','+rect['y']);
+console.log(rect['x']+', '+rect['y']);
 
 //Set the rectangle's height to be the square root of its width. (Use the 
 //`Math.sqrt()` function).
 //Use *dot notation* to access the properties!
-rect.height= Math.sqrt(rect.width);
+rect['height']= Math.sqrt(rect.width);
 
 //Log out the rectangle's area. Use *dot notation* to access the properties!
 console.log(rect.height*rect.width);
@@ -91,13 +91,14 @@ console.log(rect.height*rect.width);
 //  `cy` of 43,
 //  `radius` equal to the LAST value in the (sorted) `numbers` array.
 //Log out the circle
-let circle = { 'cx':34, 'cy':43, 'radius':numbers [numbers.length-1]};
+const circle = { 'cx':34, 'cy':43, 'radius':numbers [numbers.length-1]};
 console.log(circle);
 
 //Create an array `shapes` that represents a list of shapes. The array should
 //contain the rectangle and the circle objects defined above.
 //Log out the variable. Be sure to inspect it in the developer console!
-let shapes = [rect, circle];
+const shapes = [rect, circle];
+
 console.log(shapes);
 
 //Add a new ANONYMOUS object (e.g., one passed in directly without its own
